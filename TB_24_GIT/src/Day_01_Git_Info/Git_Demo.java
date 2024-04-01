87440cb976e726b5a3f861477f44a7613b534fa6
Admin <Admin@DESKTOP-CFDGISH>
2024-04-01 10:07:18 +0530

    By Priyanka

---
+++ TB_24_GIT/src/Day_01_Git_Info/Git_Demo.java
@@ -0,0 +1,23 @@
+package Day_01_Git_Info;
+
+public class Git_Demo 
+{
+	public void m1()
+	{
+		
+		System.out.println("In Git M1");
+	}
+
+	public void m2()
+	{
+		
+		System.out.println("In Git M2");
+	}
+	
+	public static void main(String[] args) 
+	{
+		
+	
+	}
+
+}
